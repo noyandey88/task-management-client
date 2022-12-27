23ca3d9331d00774904592d7a141084ac78f648a
codernoyan <coder.noyan@gmail.com>
2022-12-27 19:47:30 +0600

    home page added

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,9 +1,12 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react';
+import taskImage from '../../assets/task.png';
 
 const Home = () => {
   return (
-    <div className="h-screen flex justify-center items-center">
-      <h1 className="text-2xl">Welcome to Task Management App</h1>
+    <div className="flex justify-center items-center flex-col">
+      <img src={taskImage} alt="task-image" />
+      <h1 className="text-2xl font-semibold">Welcome to Task Management App</h1>
     </div>
   );
 };
