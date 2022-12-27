d7e90e6def52c9e88569599808f2861439725bd7
codernoyan <coder.noyan@gmail.com>
2022-12-27 13:37:07 +0600

    project initialized with react router and firebase

---
+++ src/firebase/firebase.config.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDKUNOUg0W0cqjPVVyEJLPj5c_xfrYHy2o",
+  authDomain: "task-management-41fea.firebaseapp.com",
+  projectId: "task-management-41fea",
+  storageBucket: "task-management-41fea.appspot.com",
+  messagingSenderId: "1054933401784",
+  appId: "1:1054933401784:web:772f06a268ee0a1c9c344d"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+// export default app;
