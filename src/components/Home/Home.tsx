31cb6df076cf18cf4e8ac2fc0908e18b99d35aee
codernoyan <coder.noyan@gmail.com>
2022-12-28 10:55:20 +0600

    added add task button to home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,8 +5,11 @@ import taskImage from '../../assets/task.png';
 const Home = () => {
   return (
     <div className="flex justify-center items-center flex-col">
-      <img src={taskImage} alt="task-image" />
+      <img src={taskImage} className="h-96" alt="task-image" />
       <h1 className="text-2xl font-semibold">Welcome to Task Management App</h1>
+      <div className="mt-4">
+        <button className="bg-indigo-600 hover:bg-indigo-700 text-white px-6 py-2 font-semibold rounded-md">Add Task</button>
+      </div>
     </div>
   );
 };
