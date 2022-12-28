329d68b4a125afb2391a193cef4117a2339ce718
codernoyan <coder.noyan@gmail.com>
2022-12-29 02:59:21 +0600

    implemented task details route

--- src/components/TaskDetails/TaskDetails.tsx
+++ src/components/TaskDetails/TaskDetails.tsx
@@ -1,9 +1,26 @@
+// @ts-nocheck
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const TaskDetails = () => {
+  const taskDetails = useLoaderData();
+  const { title, description, userEmail, imageData, postTime } = taskDetails;
   return (
-    <div>
-      <h2>hello from task details</h2>
+    <div className="w-full md:w-1/2 md:mx-auto px-2 md:px-0">
+      <div className="mb-4">
+        <h2 className="text-2xl font-semibold">Task Details</h2>
+      </div>
+      <div className="md:flex gap-4">
+        <div>
+          <img className="h-60 object-cover" src={imageData} alt="taskImage" />
+        </div>
+        <div>
+          <h4>User: {userEmail}</h4>
+          <h2>Title: {title}</h2>
+          <p>Description: {description}</p>
+          <p>Posted On: {postTime ? postTime : "N/A"}</p>
+        </div>
+      </div>
     </div>
   );
 };
