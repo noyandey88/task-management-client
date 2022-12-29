0087e85e10df6bf648c0efdb6e26ea6af30b7752
codernoyan <coder.noyan@gmail.com>
2022-12-29 13:40:48 +0600

    styles added in media route

--- src/components/Media/MediaTask.tsx
+++ src/components/Media/MediaTask.tsx
@@ -14,9 +14,13 @@ type MediaTaskProps = {
 const MediaTask = ({ task }: MediaTaskProps) => {
   const { imageData, title, status } = task;
   return (
-    <div>
+    <div className='md:flex gap-4 items-center bg-gray-100 my-4 p-1 rounded-sm'>
       <div>
-        <img src={imageData} alt="imageData" />
+        <img className="w-full h-60 md:w-60 md:h-60 object-cover" src={imageData} alt="imageData" />
+      </div>
+      <div className="mt-2 md:mt-0">
+        <h2 className="font-semibold">Task Title: <span className="capitalize">{title}</span></h2>
+        <p className="font-medium">Status: <span className="capitalize text-indigo-600">{status}</span></p>
       </div>
     </div>
   );
