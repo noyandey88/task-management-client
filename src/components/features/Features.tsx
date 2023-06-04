e3ee0cc09ed9019585493ef879f75b640be78d8e
codernoyan <coder.noyan@gmail.com>
2023-06-04 23:51:29 +0600

    project completed

--- src/components/features/Features.tsx
+++ src/components/features/Features.tsx
@@ -46,10 +46,10 @@ export default function Features() {
             <p className="text-gray-500 dark:text-gray-300">
               Minimalistic Elegance: Our designs embrace minimalism, focusing on essential elements and eliminating clutter.
             </p>
-            <a href="#" className="flex items-center -mx-1 text-sm text-blue-500 capitalize transition-colors duration-300 transform dark:text-blue-400 hover:underline hover:text-blue-600 dark:hover:text-blue-500">
+            <Link to="/" className="flex items-center -mx-1 text-sm text-blue-500 capitalize transition-colors duration-300 transform dark:text-blue-400 hover:underline hover:text-blue-600 dark:hover:text-blue-500">
               <span className="mx-1">read more</span>
               <svg className="w-4 h-4 mx-1 rtl:-scale-x-100" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd" /></svg>
-            </a>
+            </Link>
           </div>
         </div>
       </div>
